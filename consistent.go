dfa821796336c401cb55c57b9dfd21a7cd9c2f6f
myyang <ymy1019@gmail.com>
2016-07-18 21:02:23 +0800

    First commit

---
+++ consistent.go
@@ -0,0 +1,232 @@
+// Package consistent provides ...
+package consistent
+
+import (
+	"fmt"
+	"hash/crc64"
+	"hash/fnv"
+	"sort"
+	"sync"
+)
+
+// Default constants
+const (
+	DefaultReplica = 100
+	CRC64ECMA128   = 0xC96C5795D7870F42
+)
+
+// default variables
+var (
+	CRC64ECMA128Table = crc64.MakeTable(CRC64ECMA128)
+)
+
+// NewConsistent return new consistent with default value, replica number: 100 and hash algo: crc64
+func NewConsistent() *Consistent {
+	return NewConsistentWithN(DefaultReplica)
+}
+
+// NewConsistentWithN return consistent with given replica number and defautl hash algo: crc64
+func NewConsistentWithN(replicas int) *Consistent {
+	return NewConsistentWithHash(replicas, crc64h)
+}
+
+// NewConsistentWithHash return consistent with given hash algorithm
+func NewConsistentWithHash(replicas int, fn HashFunc) *Consistent {
+	c := &Consistent{}
+	c.node = make(map[string]bool)
+	c.nodesmap = make(map[uint64]string)
+	c.setReplica(replicas)
+	c.setHashFunc(fn)
+	return c
+}
+
+// HashFunc provides flexibility to give desired hash algorithm
+type HashFunc func([]byte) uint64
+
+func fnvh(key []byte) uint64 {
+	// not balanced while compute
+	// pending for verifying
+	cryptor := fnv.New64a()
+	cryptor.Write(key)
+	return cryptor.Sum64()
+}
+
+func crc64h(key []byte) uint64 {
+	return crc64.Checksum(key, CRC64ECMA128Table)
+}
+
+type suint64 []uint64
+
+func (s suint64) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s suint64) Len() int           { return len(s) }
+func (s suint64) Less(i, j int) bool { return s[i] < s[j] }
+
+type consistentError struct {
+	Msg string
+}
+
+func (c consistentError) Error() string {
+	return fmt.Sprintf("Consistent Error, %v\n", c.Msg)
+}
+
+// Consistent struct
+type Consistent struct {
+	mu       sync.RWMutex
+	count    int
+	node     map[string]bool
+	nodesmap map[uint64]string
+	nodeskey suint64
+	replicas int
+	hashfunc HashFunc
+}
+
+func (c *Consistent) setReplica(n int) {
+	// at least one node, hide this from client
+	if n <= 0 {
+		n = 1
+	}
+	c.replicas = n
+}
+
+func (c *Consistent) setHashFunc(fn HashFunc) {
+	c.hashfunc = fn
+}
+
+func (c *Consistent) hashKey(key []byte, i int) uint64 {
+	for i > 0 {
+		j := byte(i % 256)
+		i /= 256
+		key = append(key, j)
+	}
+	return c.hashfunc(key)
+}
+
+// AddNode to consistent
+func (c *Consistent) AddNode(node string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if _, ok := c.node[node]; ok {
+		return
+	}
+	nodeByte := []byte(node)
+	for i := 0; i < c.replicas; i++ {
+		key := c.hashKey(nodeByte, i)
+		c.nodesmap[key] = node
+		c.nodeskey = append(c.nodeskey, key)
+	}
+	sort.Sort(c.nodeskey)
+	c.node[node] = true
+	c.count++
+}
+
+// AddNodes provides shortcut to add multiple nodes
+func (c *Consistent) AddNodes(nodes []string) {
+	for _, n := range nodes {
+		c.AddNode(n)
+	}
+}
+
+// RemoveNode from consistent
+func (c *Consistent) RemoveNode(node string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if _, ok := c.node[node]; !ok {
+		return
+	}
+	nodeByte := []byte(node)
+	for i := 0; i < c.replicas; i++ {
+		key := c.hashKey(nodeByte, i)
+		delete(c.nodesmap, key)
+		c.remove(key)
+	}
+	delete(c.node, node)
+	c.count--
+}
+
+// RemoveNodes provides shortcut to remove nodes
+func (c *Consistent) RemoveNodes(nodes []string) {
+	for _, n := range nodes {
+		c.RemoveNode(n)
+	}
+}
+
+func (c *Consistent) remove(key uint64) {
+	i := c.search(key)
+	c.nodeskey = append(c.nodeskey[:i], c.nodeskey[i+1:]...)
+}
+
+// GetNode returns first found node
+func (c *Consistent) GetNode(key string) (string, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	if len(c.nodeskey) == 0 {
+		return "", consistentError{Msg: "Empty! No nodes."}
+	}
+	ind := c.searchKey(key)
+	node := c.getNode(ind)
+	return node, nil
+}
+
+func (c *Consistent) search(key uint64) int {
+	ind := sort.Search(len(c.nodeskey), func(i int) bool { return c.nodeskey[i] >= key })
+	if ind >= len(c.nodeskey) {
+		ind = 0
+	}
+	return ind
+}
+
+func (c *Consistent) searchKey(key string) int {
+	return c.search(c.hashfunc([]byte(key)))
+}
+
+// GetNNode returns found distinct nodes with given n
+func (c *Consistent) GetNNode(key string, n int) ([]string, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	if n > c.count {
+		return []string{}, consistentError{Msg: "Query N is greater than total nodes"}
+	}
+	nodes := make([]string, 0)
+	ind, max := c.searchKey(key), c.replicas*c.count-1
+	for len(nodes) < n {
+		if t := c.getNode(ind); !stringInSlice(nodes, t) {
+			nodes = append(nodes, t)
+		}
+		if ind < max {
+			ind++
+		} else {
+			ind = 0
+		}
+	}
+	return nodes, nil
+}
+
+func stringInSlice(l []string, x string) bool {
+	for _, s := range l {
+		if s == x {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Consistent) getNode(ind int) string {
+	return c.nodesmap[c.nodeskey[ind]]
+}
+
+// Get3Node is shortcut to get 3 Node
+// Becasue 3 replica/sharding is a practical number for performance and robust
+func (c *Consistent) Get3Node(key string) ([]string, error) {
+	return c.GetNNode(key, 3)
+}
+
+// HasNode tests exsiting node
+func (c *Consistent) HasNode(node string) bool {
+	_, ok := c.node[node]
+	return ok
+}
+
+// NodeNumber return currently physical node number
+func (c *Consistent) NodeNumber() int {
+	return c.count
+}
---
+++ consistent_test.go
@@ -0,0 +1,129 @@
+package consistent
+
+import "fmt"
+import "reflect"
+import "testing"
+
+func TestInit(t *testing.T) {
+	_ = NewConsistent()
+	_ = NewConsistentWithN(200)
+	_ = NewConsistentWithHash(130, func([]byte) uint64 {
+		return 0
+	})
+}
+
+func TestNodeOperation(t *testing.T) {
+	c := NewConsistent()
+
+	c.AddNodes([]string{"192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4", "192.168.1.5"})
+
+	if c.NodeNumber() != 5 {
+		t.Errorf("Wrong NodeNumber(), exp: 2, got %v\n", c.NodeNumber())
+	}
+
+	c.RemoveNode("192.168.1.3")
+
+	if c.NodeNumber() != 4 {
+		t.Errorf("Wrong NodeNumber(), exp: 1, got %v\n", c.NodeNumber())
+	}
+
+	c.RemoveNodes([]string{"192.168.1.3", "192.168.1.2", "192.168.1.5"})
+
+	if c.NodeNumber() != 2 {
+		t.Errorf("Wrong NodeNumber(), exp: 1, got %v\n", c.NodeNumber())
+	}
+
+	testHasNode := []struct {
+		Addr string
+		Exp  bool
+		Msg  string
+	}{
+		{"192.168.1.1", true, "Can't found 192.168.1.1"},
+		{"192.168.1.2", false, "Found 192.168.1.2"},
+		{"192.168.1.3", false, "Found 192.168.1.3"},
+	}
+
+	for _, v := range testHasNode {
+		if c.HasNode(v.Addr) != v.Exp {
+			t.Errorf("HasNode err: %v\n", v.Msg)
+		}
+	}
+
+}
+
+func TestConsistentHashing(t *testing.T) {
+	c := NewConsistent()
+	c.AddNodes([]string{"node1", "node2", "node3", "node4", "node5"})
+
+	testGetNode := []struct {
+		Key string
+		Exp string
+		Msg string
+	}{
+		{"Abc", "node1", "Wrong mapping Abc -> node1"},
+		{"xxx", "node1", "Wrong mapping xxx -> node2"},
+		{"1111234567", "node5", "Wrong mapping 1111234567 -> node5"},
+		{"okbnqeobla;d", "node2", "Wrong mapping okbnqeobla;d -> node2"},
+	}
+
+	for _, v := range testGetNode {
+		if node, err := c.GetNode(v.Key); err != nil || node != v.Exp {
+			t.Errorf("GetNode err: %v, exp: %v, got: %v\n", v.Msg, v.Exp, node)
+		}
+	}
+
+	testGetNnode := []struct {
+		Key string
+		N   int
+		Exp []string
+		Err error
+		Msg string
+	}{
+		{"Abc", 1, []string{"node1"}, nil, "Get 1 Wrong mapping Abc -> node1"},
+		{"xxx", 2, []string{"node1", "node2"}, nil, "Get 2 Wrong mapping xxx -> node1, 2"},
+		{"okbnqeobla;d", 6, []string{}, consistentError{Msg: "Query N is greater than total nodes"},
+			"Get N greater than total node is invalid"},
+	}
+
+	for _, v := range testGetNnode {
+		if node, err := c.GetNNode(v.Key, v.N); err != v.Err || !reflect.DeepEqual(node, v.Exp) {
+			t.Errorf("GetNNode err: %v, exp: %v, got: %v\n", v.Msg, v.Exp, node)
+		}
+	}
+
+	c.RemoveNodes([]string{"node1", "node2", "node3", "node4", "node5"})
+
+	testEmpty := []struct {
+		Key string
+		Exp error
+		Msg string
+	}{
+		{"okbnqeobla;d", consistentError{Msg: "Empty! No nodes."}, "Empty! No nodes."},
+	}
+
+	for _, v := range testEmpty {
+		if _, err := c.GetNode(v.Key); err != v.Exp {
+			t.Errorf("GetNode err: %v, exp: %v, got: %v\n", v.Msg, v.Exp, err)
+		}
+	}
+
+}
+
+func BenchmarkAddAndRemove(b *testing.B) {
+	b.ReportAllocs()
+	c := NewConsistent()
+	nodes := []string{"n1", "n2", "n3", "n4", "n5", "n6", "n7", "n8", "n9", "n10", "n11", "n12"}
+	for i := 0; i < b.N; i++ {
+		c.AddNodes(nodes)
+		c.RemoveNodes(nodes)
+	}
+}
+
+func BenchmarkGetNNode(b *testing.B) {
+	b.ReportAllocs()
+	c := NewConsistent()
+	c.AddNodes([]string{"n1", "n2", "n3", "n4", "n5", "n6", "n7", "n8", "n9", "n10", "n11", "n12"})
+	for i := 0; i < b.N; i++ {
+		c.GetNNode(fmt.Sprintf("%v", i), 5)
+	}
+}
